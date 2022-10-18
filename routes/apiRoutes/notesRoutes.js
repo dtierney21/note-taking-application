65ecd0f06c2d67e68cbd49f8e81cb49a30fc88e2
Dustin Tierney <dustintierney@rockethomes.com>
2022-10-18 12:58:27 -0400

    added the notes post route

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,10 +1,34 @@
 const router = require('express').Router();
 const notes = require('../../db/db');
+const fs = require('fs');
 
 // retrieve exisitng notes from db
 router.get('/notes', (req, res) => {
-    let results = notes;
+    const results = notes;
     res.json(results);
+});
+
+// Cretae a new note and add it to the exiting array of notes in the db.json file
+router.post('/notes', (req, res) => {
+    // Use fs to the db.json file
+    fs.readFile('db/db.json', (err, data) => {
+        let notes = JSON.parse(data);
+        // get the note data from the request
+        const note = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuidv1(),
+        };
+        console.log(note);
+        res.json(note);
+
+        // Add data to the notes file
+        notes.push(note);
+        console.log(notes);
+
+        // Write the new array to the db.json file
+        fs.writeFile('db/db.json', JSON.stringify(notes, null, 2));
+    });
 });
 
 module.exports = router;
